628c39264410a15286dcf8666495c17184087631
Burak Filiz <dev.burakfiliz@gmail.com>
2023-02-20 14:18:27 +0300

    Backend connection complete

---
+++ src/app/models/rental.ts
@@ -0,0 +1,7 @@
+export interface Rental{
+ carId : number;
+ brandName : string;
+ customerName:string;
+ rentDate : Date;
+ returnDate: Date;
+}
--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RentalResponseModel } from '../models/rentalResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
+apiUrl = 'https://localhost:44392/api/Renals/getrental';
+  constructor(private httpClient : HttpClient) { }
 
-  constructor() { }
+  getRentals():Observable<RentalResponseModel>{
+    return this.httpClient.get<RentalResponseModel>(this.apiUrl)
+  }
 }
